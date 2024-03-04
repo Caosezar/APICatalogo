465ba99d9c52c07bcb50c4c17d0e61ad276306a7
Caio César Lima Braga <caio.braga@fitbank.com.br>
2024-03-04 14:28:05 -0300

    Commit Estrutural Controller

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,24 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Platform.Util.Resourcer;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController (AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Produtos> Get()
+        {
+            var Produtos = _context.Produtos.ToList();
+        }
+    }
+}
